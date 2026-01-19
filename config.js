fa5917fc99236ff50ca6b7c0f187a89ee50e1b37
dinesh madusanka <dineshofcai@gmail.com>
2026-01-19 09:43:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,9 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-PORT: process.env.PORT || "8000"
+    // මෙතන 'YOUR_SESSION_ID_HERE' වෙනුවට ඔබේ ID එක දාන්න
+    SESSION_ID: process.env.SESSION_ID || "YOUR_SESSION_ID_HERE", 
+    PORT: process.env.PORT || "8000"
 };
